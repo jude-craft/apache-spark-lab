7542e9478f435cc710e583949ae2aa95d0b329ee
jude-craft <derekjude254@gmail.com>
2026-09-19 05:42:58 +0300

    feat: added download_data.sh

---
+++ scripts/download_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+TAXI_TYPE=$1 # "yellow" or "green"
+YEAR=$2 # e.g., 2020
+
+# The modern NYC TLC CloudFront URL for Parquet files
+URL_PREFIX="https://d37ci6vzurychx.cloudfront.net/trip-data"
+
+for MONTH in {1..12}; do
+  FMONTH=`printf "%02d" ${MONTH}`
+
+  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
+
+  LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet"
+  LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+  echo "Downloading ${URL} to${LOCAL_PATH}"
+  mkdir -p ${LOCAL_PREFIX}
+  
+  # Download the file
+  wget ${URL} -O${LOCAL_PATH}
+done
